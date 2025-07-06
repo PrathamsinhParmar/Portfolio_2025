57e2821883453f697855d38b1b4610767380b1e5
Pratham <prathamsinhparmar0@gmail.com>
2025-07-06 12:02:26 +0530

    Working form with popup massage

--- script.js
+++ script.js
@@ -21,25 +21,3 @@
 
 
 
-    const form = document.querySelector('.contact-form');
-    const status = document.getElementById('form-status');
-
-    form.addEventListener("submit", async function (e) {
-        e.preventDefault();
-        const data = new FormData(form);
-        const action = form.action;
-        const response = await fetch(action, {
-            method: "POST",
-            body: data,
-            headers: {
-                'Accept': 'application/json'
-            }
-        });
-
-        if (response.ok) {
-            status.textContent = "Thank you! Your message has been sent.";
-            form.reset();
-        } else {
-            status.textContent = "Oops! There was a problem submitting your form.";
-        }
-    });
